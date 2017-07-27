b71a1d67e4420c63f7e21f278397911c60ff1969
Wynne M Lo <wynnemlo@gmail.com>
2017-07-27 12:21:10 +0800

    first commit

---
+++ Tippy/TipViewController.swift
@@ -0,0 +1,47 @@
+//
+//  TipViewController.swift
+//  Tippy
+//
+//  Created by Wynne M Lo on 21/7/2017.
+//  Copyright © 2017 Wynne Lo. All rights reserved.
+//
+
+import UIKit
+
+class TipViewController: UIViewController {
+
+    // MARK: - View Cycles
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        print("view will appear")
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        print("view did appear")
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        print("view will disappear")
+    }
+    
+    override func viewDidDisappear(_ animated: Bool) {
+        super.viewDidDisappear(animated)
+        print("view did disappear")
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+
+}
+
